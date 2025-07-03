a3ef6f087a83359523faf1302674f2e5de3dae32
Peter Ayad <eng.peter2013@gmail.com>
2025-07-03 07:27:56 -0700

    Write Complex Queries with Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,7 +4,8 @@
 
 SELECT b.booking_id, u.first_name, u.last_name, u.email, b.start_date, b.end_date, b.total_price 
 FROM Booking b
-INNER JOIN 'User' u ON b.user_id = u.user_id;
+INNER JOIN 'User' u ON b.user_id = u.user_id
+ORDER BY b.start_date DESC;
 
 -- ====================================================================================================
 -- 2- LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews
@@ -12,7 +13,8 @@ INNER JOIN 'User' u ON b.user_id = u.user_id;
 
 SELECT p.property_id, p.name, p.description, p.location, p.price_per_night, r.rating, r.comment
 FROM Property p
-LEFT JOIN Review r ON p.property_id = r.property_id;
+LEFT JOIN Review r ON p.property_id = r.property_id
+ORDER BY p.name ASC;
 
 -- ===============================================================================================================================
 -- 3- FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
@@ -27,4 +29,6 @@ SELECT u.user_id, u.first_name, u.last_name, u.email, u.phone_number, u.role,
        b.booking_id, b.property_id, b.start_date, b.end_date, b.total_price, b.status
 FROM 'User' u
 RIGHT JOIN Booking b ON u.user_id = b.user_id;
+WHERE u.user_id = NULL
+ORDER BY u.first_name, b.booking_id;
 
